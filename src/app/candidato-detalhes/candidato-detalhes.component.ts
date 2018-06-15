08edd16a813486c9d3620849baf2abf746545749
Carol Bastos <acbl.ads@uea.edu.br>
2018-06-14 20:28:02 -0400

    Add pag Sobre, Candidatos Recomendados e Contatos

---
+++ src/app/candidato-detalhes/candidato-detalhes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eqv-candidato-detalhes',
+  templateUrl: './candidato-detalhes.component.html',
+  styleUrls: ['./candidato-detalhes.component.css']
+})
+export class CandidatoDetalhesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/tema-subtemas/tema-subtemas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eqv-tema-subtemas',
+  templateUrl: './tema-subtemas.component.html',
+  styleUrls: ['./tema-subtemas.component.css']
+})
+export class TemaSubtemasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
